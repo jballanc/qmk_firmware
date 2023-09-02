f3db1abe25405e11c29512537f70b9b2352103e1
Josh Ballanco <josh.ballanco@manhattanmetric.com>
2023-09-02 16:35:48 -0400

    Add layouts for Iris split

---
+++ keyboards/keebio/iris/keymaps/jballanc/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// Prevent Mod/Layer-Tap from registering two keys when typing fast
+#define PERMISSIVE_HOLD
+
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
+
+#define MK_KINETIC_SPEED
+
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+#define NO_ACTION_ONESHOT
